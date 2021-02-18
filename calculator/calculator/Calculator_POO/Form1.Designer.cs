c0f0c542710e1baf3d2eaeab20bd05ec1611f230
Carmen <68381000+Carmenbb@users.noreply.github.com>
2021-02-18 15:12:12 +0200

    Add files via upload

---
+++ calculator/calculator/Calculator_POO/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace Calculator_POO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.B3 = new System.Windows.Forms.Button();
+            this.B5 = new System.Windows.Forms.Button();
+            this.ClearB = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B0 = new System.Windows.Forms.Button();
+            this.B4 = new System.Windows.Forms.Button();
+            this.VirgulaB = new System.Windows.Forms.Button();
+            this.B6 = new System.Windows.Forms.Button();
+            this.B9 = new System.Windows.Forms.Button();
+            this.B8 = new System.Windows.Forms.Button();
+            this.B7 = new System.Windows.Forms.Button();
+            this.EgalB = new System.Windows.Forms.Button();
+            this.SumaB = new System.Windows.Forms.Button();
+            this.MinusB = new System.Windows.Forms.Button();
+            this.inmultireB = new System.Windows.Forms.Button();
+            this.impartireB = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // B3
+            // 
+            this.B3.BackColor = System.Drawing.Color.RosyBrown;
+            this.B3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B3.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B3.Location = new System.Drawing.Point(124, 231);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(50, 50);
+            this.B3.TabIndex = 35;
+            this.B3.Text = "3";
+            this.B3.UseVisualStyleBackColor = false;
+            this.B3.Click += new System.EventHandler(this.B3_Click);
+            // 
+            // B5
+            // 
+            this.B5.BackColor = System.Drawing.Color.RosyBrown;
+            this.B5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B5.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B5.Location = new System.Drawing.Point(68, 175);
+            this.B5.Name = "B5";
+            this.B5.Size = new System.Drawing.Size(50, 50);
+            this.B5.TabIndex = 34;
+            this.B5.Text = "5";
+            this.B5.UseVisualStyleBackColor = false;
+            this.B5.Click += new System.EventHandler(this.B5_Click);
+            // 
+            // ClearB
+            // 
+            this.ClearB.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClearB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ClearB.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClearB.Location = new System.Drawing.Point(12, 63);
+            this.ClearB.Name = "ClearB";
+            this.ClearB.Size = new System.Drawing.Size(50, 50);
+            this.ClearB.TabIndex = 33;
+            this.ClearB.Text = "C";
+            this.ClearB.UseVisualStyleBackColor = false;
+            this.ClearB.Click += new System.EventHandler(this.ClearB_Click);
+            // 
+            // B2
+            // 
+            this.B2.BackColor = System.Drawing.Color.RosyBrown;
+            this.B2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B2.Location = new System.Drawing.Point(68, 231);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(50, 50);
+            this.B2.TabIndex = 32;
+            this.B2.Text = "2";
+            this.B2.UseVisualStyleBackColor = false;
+            this.B2.Click += new System.EventHandler(this.B2_Click);
+            // 
+            // B0
+            // 
+            this.B0.BackColor = System.Drawing.Color.RosyBrown;
+            this.B0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B0.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B0.Location = new System.Drawing.Point(68, 63);
+            this.B0.Name = "B0";
+            this.B0.Size = new System.Drawing.Size(50, 50);
+            this.B0.TabIndex = 31;
+            this.B0.Text = "0";
+            this.B0.UseVisualStyleBackColor = false;
+            this.B0.Click += new System.EventHandler(this.B0_Click);
+            // 
+            // B4
+            // 
+            this.B4.BackColor = System.Drawing.Color.RosyBrown;
+            this.B4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B4.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B4.Location = new System.Drawing.Point(12, 175);
+            this.B4.Name = "B4";
+            this.B4.Size = new System.Drawing.Size(50, 50);
+            this.B4.TabIndex = 30;
+            this.B4.Text = "4";
+            this.B4.UseVisualStyleBackColor = false;
+            this.B4.Click += new System.EventHandler(this.B4_Click);
+            // 
+            // VirgulaB
+            // 
+            this.VirgulaB.BackColor = System.Drawing.Color.RosyBrown;
+            this.VirgulaB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.VirgulaB.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VirgulaB.Location = new System.Drawing.Point(124, 63);
+            this.VirgulaB.Name = "VirgulaB";
+            this.VirgulaB.Size = new System.Drawing.Size(50, 50);
+            this.VirgulaB.TabIndex = 29;
+            this.VirgulaB.Text = ",";
+            this.VirgulaB.UseVisualStyleBackColor = false;
+            this.VirgulaB.Click += new System.EventHandler(this.VirgulaB_Click);
+            // 
+            // B6
+            // 
+            this.B6.BackColor = System.Drawing.Color.RosyBrown;
+            this.B6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B6.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B6.Location = new System.Drawing.Point(124, 175);
+            this.B6.Name = "B6";
+            this.B6.Size = new System.Drawing.Size(50, 50);
+            this.B6.TabIndex = 28;
+            this.B6.Text = "6";
+            this.B6.UseVisualStyleBackColor = false;
+            this.B6.Click += new System.EventHandler(this.B6_Click);
+            // 
+            // B9
+            // 
+            this.B9.BackColor = System.Drawing.Color.RosyBrown;
+            this.B9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B9.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B9.Location = new System.Drawing.Point(124, 119);
+            this.B9.Name = "B9";
+            this.B9.Size = new System.Drawing.Size(50, 50);
+            this.B9.TabIndex = 27;
+            this.B9.Text = "9";
+            this.B9.UseVisualStyleBackColor = false;
+            this.B9.Click += new System.EventHandler(this.B9_Click);
+            // 
+            // B8
+            // 
+            this.B8.BackColor = System.Drawing.Color.RosyBrown;
+            this.B8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B8.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B8.Location = new System.Drawing.Point(68, 119);
+            this.B8.Name = "B8";
+            this.B8.Size = new System.Drawing.Size(50, 50);
+            this.B8.TabIndex = 26;
+            this.B8.Text = "8";
+            this.B8.UseVisualStyleBackColor = false;
+            this.B8.Click += new System.EventHandler(this.B8_Click);
+            // 
+            // B7
+            // 
+            this.B7.BackColor = System.Drawing.Color.RosyBrown;
+            this.B7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B7.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B7.Location = new System.Drawing.Point(12, 119);
+            this.B7.Name = "B7";
+            this.B7.Size = new System.Drawing.Size(50, 50);
+            this.B7.TabIndex = 25;
+            this.B7.Text = "7";
+            this.B7.UseVisualStyleBackColor = false;
+            this.B7.Click += new System.EventHandler(this.B7_Click);
+            // 
+            // EgalB
+            // 
+            this.EgalB.BackColor = System.Drawing.Color.RosyBrown;
+            this.EgalB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.EgalB.Location = new System.Drawing.Point(12, 287);
+            this.EgalB.Name = "EgalB";
+            this.EgalB.Size = new System.Drawing.Size(218, 23);
+            this.EgalB.TabIndex = 24;
+            this.EgalB.Text = "=";
+            this.EgalB.UseVisualStyleBackColor = false;
+            this.EgalB.Click += new System.EventHandler(this.EgalB_Click);
+            // 
+            // SumaB
+            // 
+            this.SumaB.BackColor = System.Drawing.Color.RosyBrown;
+            this.SumaB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SumaB.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SumaB.Location = new System.Drawing.Point(180, 175);
+            this.SumaB.Name = "SumaB";
+            this.SumaB.Size = new System.Drawing.Size(50, 50);
+            this.SumaB.TabIndex = 23;
+            this.SumaB.Text = "+";
+            this.SumaB.UseVisualStyleBackColor = false;
+            this.SumaB.Click += new System.EventHandler(this.SumaB_Click);
+            // 
+            // MinusB
+            // 
+            this.MinusB.BackColor = System.Drawing.Color.RosyBrown;
+            this.MinusB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MinusB.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MinusB.Location = new System.Drawing.Point(180, 231);
+            this.MinusB.Name = "MinusB";
+            this.MinusB.Size = new System.Drawing.Size(50, 50);
+            this.MinusB.TabIndex = 22;
+            this.MinusB.Text = "-";
+            this.MinusB.UseVisualStyleBackColor = false;
+            this.MinusB.Click += new System.EventHandler(this.MinusB_Click);
+            // 
+            // inmultireB
+            // 
+            this.inmultireB.BackColor = System.Drawing.Color.RosyBrown;
+            this.inmultireB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.inmultireB.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inmultireB.Location = new System.Drawing.Point(180, 119);
+            this.inmultireB.Name = "inmultireB";
+            this.inmultireB.Size = new System.Drawing.Size(50, 50);
+            this.inmultireB.TabIndex = 21;
+            this.inmultireB.Text = "*";
+            this.inmultireB.UseVisualStyleBackColor = false;
+            this.inmultireB.Click += new System.EventHandler(this.inmultireB_Click);
+            // 
+            // impartireB
+            // 
+            this.impartireB.BackColor = System.Drawing.Color.RosyBrown;
+            this.impartireB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.impartireB.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.impartireB.Location = new System.Drawing.Point(180, 63);
+            this.impartireB.Name = "impartireB";
+            this.impartireB.Size = new System.Drawing.Size(50, 50);
+            this.impartireB.TabIndex = 20;
+            this.impartireB.Text = ":";
+            this.impartireB.UseVisualStyleBackColor = false;
+            this.impartireB.Click += new System.EventHandler(this.impartireB_Click);
+            // 
+            // B1
+            // 
+            this.B1.BackColor = System.Drawing.Color.RosyBrown;
+            this.B1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B1.Location = new System.Drawing.Point(12, 231);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(50, 50);
+            this.B1.TabIndex = 19;
+            this.B1.Text = "1";
+            this.B1.UseVisualStyleBackColor = false;
+            this.B1.Click += new System.EventHandler(this.B1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.MistyRose;
+            this.textBox1.Font = new System.Drawing.Font("Yu Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(218, 45);
+            this.textBox1.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.ClientSize = new System.Drawing.Size(247, 329);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.B5);
+            this.Controls.Add(this.ClearB);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B0);
+            this.Controls.Add(this.B4);
+            this.Controls.Add(this.VirgulaB);
+            this.Controls.Add(this.B6);
+            this.Controls.Add(this.B9);
+            this.Controls.Add(this.B8);
+            this.Controls.Add(this.B7);
+            this.Controls.Add(this.EgalB);
+            this.Controls.Add(this.SumaB);
+            this.Controls.Add(this.MinusB);
+            this.Controls.Add(this.inmultireB);
+            this.Controls.Add(this.impartireB);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button B5;
+        private System.Windows.Forms.Button ClearB;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B0;
+        private System.Windows.Forms.Button B4;
+        private System.Windows.Forms.Button VirgulaB;
+        private System.Windows.Forms.Button B6;
+        private System.Windows.Forms.Button B9;
+        private System.Windows.Forms.Button B8;
+        private System.Windows.Forms.Button B7;
+        private System.Windows.Forms.Button EgalB;
+        private System.Windows.Forms.Button SumaB;
+        private System.Windows.Forms.Button MinusB;
+        private System.Windows.Forms.Button inmultireB;
+        private System.Windows.Forms.Button impartireB;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ calculator/calculator/Calculator_POO/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_POO
+{
+    public partial class Form1 : Form
+    {
+        string input = string.Empty;
+        string operand1 = string.Empty;
+        string operand2 = string.Empty;
+        char operatie;
+        double rezultat = 0.0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void B0_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "0";
+            this.textBox1.Text +=input;
+        }
+
+        private void B1_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "1";
+            this.textBox1.Text += input;
+        }
+
+        private void B2_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "2";
+            this.textBox1.Text += input;
+        }
+
+        private void B3_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "3";
+            this.textBox1.Text += input;
+        }
+
+        private void B4_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "4";
+            this.textBox1.Text += input;
+        }
+
+        private void B5_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "5";
+            this.textBox1.Text += input;
+        }
+
+        private void B6_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "6";
+            this.textBox1.Text += input;
+        }
+
+        private void B7_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "7";
+            this.textBox1.Text += input;
+        }
+
+        private void B8_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "8";
+            this.textBox1.Text += input;
+        }
+
+        private void B9_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "9";
+            this.textBox1.Text += input;
+        }
+
+        private void EgalB_Click(object sender, EventArgs e)
+        {
+           
+          
+            operand2 = input;
+            double num1, num2;
+            double.TryParse(operand1, out num1);
+            double.TryParse(operand2, out num2);
+
+            this.textBox1.Text = "";
+            this.input = string.Empty;
+            this.operand1 = string.Empty;
+            this.operand2 = string.Empty;
+
+            if (operatie == '+')
+            {
+                rezultat = num1 + num2;
+                textBox1.Text = rezultat.ToString();
+            }
+            else if (operatie == '-')
+            {
+                rezultat = num1 - num2;
+                textBox1.Text = rezultat.ToString();
+            }
+            else if (operatie == '*')
+            {
+                rezultat = num1 * num2;
+                textBox1.Text = rezultat.ToString();
+            }
+            else if (operatie == '/')
+            {
+                if (num2 != 0)
+                {
+                    rezultat = num1 / num2;
+                    textBox1.Text = rezultat.ToString();
+                }
+                else
+                {
+                    textBox1.Text = "error";
+                }
+
+            }
+
+        }
+
+        private void MinusB_Click(object sender, EventArgs e)
+        {
+            
+            operand1 = input;
+            operatie = '-';
+            input = string.Empty;
+            
+        }
+
+        private void SumaB_Click(object sender, EventArgs e)
+        {
+            
+            operand1 = input;
+            operatie = '+';
+            input = string.Empty;
+            
+        }
+
+        private void inmultireB_Click(object sender, EventArgs e)
+        {
+            
+            operand1 = input;
+            operatie = '*';
+            input = string.Empty;
+           
+        }
+
+        private void impartireB_Click(object sender, EventArgs e)
+        {
+            
+            operand1 = input;
+            operatie = '/';
+            input = string.Empty;
+            
+        }
+
+        private void VirgulaB_Click(object sender, EventArgs e)
+        {
+          
+
+            this.textBox1.Text = "";
+            input += ".";
+            this.textBox1.Text += input;
+
+        }
+
+        private void ClearB_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            this.input = string.Empty;
+            this.operand1 = string.Empty;
+            this.operand2 = string.Empty;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
